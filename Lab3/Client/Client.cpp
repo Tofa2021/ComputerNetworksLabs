6d938d9c30cff854ad2323fd72e72aa1585b1d75
Anton Latyshev <latyshev.antony@gmail.com>
2025-10-14 22:28:35 +0300

    Add Lab3

---
+++ Lab3/Client/Client.cpp
@@ -0,0 +1,49 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "ws2_32.lib")
+
+#include <iostream>
+#include <WinSock2.h>
+
+using namespace std;
+
+int main()
+{
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    wVersionRequested = MAKEWORD(2, 2);
+    if (WSAStartup(wVersionRequested, &wsaData) == WSAVERNOTSUPPORTED) {
+        cout << "WSAStartup error" << endl;
+        return -1;
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        WSACleanup();
+        cout << "Invalid socket" << endl;
+        return -1;
+    }
+
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(1280);
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        closesocket(clientSocket);
+        cout << "Client socket error" << endl;
+        return -1;
+    }
+
+    cout << "Enter string" << endl;
+    string sendBuffer;
+    cin >> sendBuffer;
+    send(clientSocket, sendBuffer.c_str(), sizeof(sendBuffer), 0);
+
+    char recivedBuffer[255];
+    memset(recivedBuffer, '\0', sizeof(recivedBuffer));
+    int recivedBytesCount = recv(clientSocket, recivedBuffer, sizeof(recivedBuffer), 0);
+    cout << "Recived: " << recivedBuffer << endl;
+
+    closesocket(clientSocket);
+    WSACleanup();
+}
---
+++ Lab3/Server/Server.cpp
@@ -0,0 +1,94 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "ws2_32.lib")
+
+#include <iostream>
+#include<thread>
+#include<vector>
+#include<WinSock2.h>
+
+using namespace std;
+
+void clientHandler(SOCKET clientSocket) {
+	char recivedBuffer[255];
+	int recivedBytesCount;
+	recivedBytesCount = recv(clientSocket, recivedBuffer, sizeof(recivedBuffer), 0);
+	cout << "Recived: " << recivedBuffer << endl;
+
+	for (int i = 0; i < strlen(recivedBuffer) - 1; i += 2) {
+		swap(recivedBuffer[i], recivedBuffer[i + 1]);
+	}
+
+	cout << "Send: " << recivedBuffer << endl;
+	send(clientSocket, recivedBuffer, strlen(recivedBuffer) + 1, 0);
+
+	closesocket(clientSocket);
+}
+
+int main()
+{
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	wVersionRequested = MAKEWORD(2, 2);
+	if (WSAStartup(wVersionRequested, &wsaData) == WSAVERNOTSUPPORTED) {
+		cout << "WSAStartup error" << endl;
+		return -1;
+	}
+
+	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (serverSocket == INVALID_SOCKET) {
+		WSACleanup();
+		cout << "Invalid socket" << endl;
+		return -1;
+	}
+
+	struct sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(1280);
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+		closesocket(serverSocket);
+		WSACleanup();
+		cout << "Binding failed" << endl;
+		return -1;
+	}
+
+	if (listen(serverSocket, SOMAXCONN)) {
+		closesocket(serverSocket);
+		WSACleanup();
+		cout << "Listening failed" << endl;
+		return -1;
+	}
+
+	int availableConnections = 10;
+	cout << "Server has been started" << endl;
+	vector<thread*> startedThreads(availableConnections);
+
+	SOCKADDR_IN clientAdress;
+	ZeroMemory(&clientAdress, sizeof(clientAdress));
+	int sizeOfAddres = sizeof(clientAdress);
+	for (size_t i = 0; i < availableConnections; i++)
+	{
+		SOCKET newConnection = accept(serverSocket, (sockaddr*)&clientAdress, &sizeOfAddres);
+		if (newConnection == INVALID_SOCKET)
+		{
+			closesocket(newConnection);
+			WSACleanup();
+			cout << "Accept failure" << endl;
+		}
+		else
+		{
+			startedThreads[i] = new thread([&]() { clientHandler(newConnection); });
+			cout << "New user connected! Thread ID:" << startedThreads[i]->get_id() << endl;
+		}
+	}
+	for (size_t i = 0; i < startedThreads.size(); i++)
+	{
+		startedThreads[i]->join();
+		delete startedThreads[i];
+	}
+
+	cout << "Server is offline!" << endl;
+	closesocket(serverSocket);
+	WSACleanup();
+}
